dca8f92d8083cd85719ff8d417189a43fcd2e0c3
santoshreddyy <108527128+santoshreddyy@users.noreply.github.com>
2023-02-16 17:14:58 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, Welcome to DevOps CI CD using jenkins on ec2.....");
+    response.end("Hello, Welcome to DevOps CI CD using jenkins on ...ec2");
 
 });
 
-var port = 3000;
+var port = 3001;
 server.listen(port);
 module.exports = server
 console.log("Server is available on Port:", port);
